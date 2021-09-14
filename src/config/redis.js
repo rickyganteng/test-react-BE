682b7dc03bc059d5f7ca8bbb4c419170704e2fbf
rickyganteng <syahputraricky2@gmail.com>
2021-09-14 19:59:48 +0700

    fixxs

--- src/config/redis.js
+++ src/config/redis.js
@@ -2,7 +2,9 @@ require('dotenv').config()
 const redis = require('redis')
 
 const client = redis.createClient({
-
+  host: process.env.REDIS_HOSTNAME,
+  port: process.env.REDIS_PORT,
+  password: process.env.REDIS_PASSWORD
 })
 
 client.on('connect', () => {
